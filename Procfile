1aa3c69b6743700815f61d1f2df8aa298bb775de
Ole Voldsæter <ole.voldsater@nsd.uib.no>
2018-04-20 15:52:03 +0200

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/tictac.jar clojure.main -m tictac.server
---
+++ src/cljs/tictac/core.cljs
@@ -0,0 +1,123 @@
+(ns tictac.core
+    (:require [reagent.core :as reagent :refer [atom]]
+              [secretary.core :as secretary :include-macros true]
+              [accountant.core :as accountant]))
+
+
+(defn flip-bits[x digits]
+    (bit-and (bit-not x) (- (.pow js/Math 2 digits) 1))
+)
+
+(defn cell-content [mask]
+  (cond
+    (= (bit-and @player-checked mask) mask) "X"
+    (= (bit-and @computer-checked mask) mask) "O"
+    :else "")
+  )
+
+(defn computer-move []
+  (let [free (flip-bits (bit-or @player-checked @computer-checked) 9)]
+    (loop [candidate-move (.pow js/Math 2 (rand-int 9))]
+      (if (= (bit-and free candidate-move) candidate-move)
+        (swap! computer-checked + candidate-move)
+        (recur (.pow js/Math 2 (rand-int 9)))
+      )
+    )
+  )
+)
+
+(defn update-result []
+  (if (= (bit-or @player-checked @computer-checked) 511)
+    (reset! result 3)
+    nil
+  )
+  )
+
+(defn check-square [mask]
+  (fn []
+    (if (not= (bit-and (bit-or @player-checked @computer-checked) mask) mask)
+      (do
+        (swap! player-checked + mask)
+        (update-result)
+        (if (= @result 0)
+          (computer-move)
+          nil
+          )
+        )
+      nil)
+    (.log js/console (str @player-checked " " @computer-checked))
+    )
+  )
+
+(def player-checked (atom 0))
+(def computer-checked (atom 0))
+(def result (atom 0))
+
+;; ------------------------
+;; Views
+(defn game-board []
+  [:table
+    [:tbody
+      [:tr
+        [:td {:on-click (check-square 1)} (cell-content 1)]
+        [:td {:on-click (check-square 2)} (cell-content 2)]
+        [:td {:on-click (check-square 4)} (cell-content 4)]]
+      [:tr
+        [:td {:on-click (check-square 8)} (cell-content 8)]
+        [:td {:on-click (check-square 16)} (cell-content 16)]
+        [:td {:on-click (check-square 32)} (cell-content 32)]]
+      [:tr
+        [:td {:on-click (check-square 64)} (cell-content 64)]
+        [:td {:on-click (check-square 128)} (cell-content 128)]
+        [:td {:on-click (check-square 256)} (cell-content 256)]]
+    ]
+  ]
+)
+
+(defn home-page []
+  [:div [:h2 "Welcome to tictac"]
+   [:div [:a {:href "/about"} "go to about page"]]
+   [:h1 (case @result
+          0 ""
+          1 "I win!"
+          2 "You win!"
+          3 "Draw...")]
+   (game-board)
+   ]
+   )
+
+(defn about-page []
+  [:div [:h2 "About tictac"]
+   [:div [:a {:href "/"} "go to the home page"]
+         [:p "This tic tac toe implementation is supposed to learn from its mistakes!"]]])
+
+;; -------------------------
+;; Routes
+
+(defonce page (atom #'home-page))
+
+(defn current-page []
+  [:div [@page]])
+
+(secretary/defroute "/" []
+  (reset! page #'home-page))
+
+(secretary/defroute "/about" []
+  (reset! page #'about-page))
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (reagent/render [current-page] (.getElementById js/document "app")))
+
+(defn init! []
+  (accountant/configure-navigation!
+    {:nav-handler
+     (fn [path]
+       (secretary/dispatch! path))
+     :path-exists?
+     (fn [path]
+       (secretary/locate-route path))})
+  (accountant/dispatch-current!)
+  (mount-root))
